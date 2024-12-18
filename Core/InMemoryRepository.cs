44703caae40bb85ea4ccb588be5ae1a59bed0773
Aleksroomgg <panaleks1310@gmail.com>
2024-12-18 11:52:44 +0300

    Add files via upload

---
+++ Core/InMemoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construction_practice_REFORGER.Core
+{
+    public class InMemoryRepository<T> : IRepository<T>
+    {
+        private readonly List<T> _items = new List<T>();
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+        public void Remove(T item)
+        {
+            _items.Remove(item);
+        }
+        public int GetAll()
+        {
+            return 0;
+        }
+    }
+}
